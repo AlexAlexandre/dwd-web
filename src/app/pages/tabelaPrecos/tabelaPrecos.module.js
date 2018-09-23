19d08906131616773b3f827cfc210965bc072ea1
Alex Alexandre <alexalexandrejr@gmail.com>
2018-09-23 12:06:57 -0300

    incluindo paginas de cadastro para tabela de preços

--- src/app/pages/tabelaPrecos/tabelaPrecos.module.js
+++ src/app/pages/tabelaPrecos/tabelaPrecos.module.js
@@ -10,11 +10,18 @@
             .state('tabelaPrecos', {
                 url: '/tabela-precos',
                 templateUrl: 'app/pages/tabelaPrecos/tabela-precos.html',
+                controller: 'TabelaPrecoCtrl',
                 title: 'Tabela de Preços',
                 sidebarMeta: {
                     icon: 'ion-grid',
                     order: 3,
                 },
+            })
+            .state('tabelaPrecos-incluir', {
+                url: '/incluir-tabela-precos',
+                templateUrl: 'app/pages/tabelaPrecos/incluir-tabela-precos.html',
+                // controller: 'IncluirFornecedoresCtrl',
+                title: 'Incluir Tabela de Preços'
             });
     }
 
