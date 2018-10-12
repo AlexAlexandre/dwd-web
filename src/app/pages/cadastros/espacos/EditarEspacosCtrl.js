cbba4b49ede110f85ea630112d3bf68482c695f1
Alex Alexandre <alexalexandrejr@gmail.com>
2018-10-12 15:31:24 -0300

    Corrigindo alguns erros

--- src/app/pages/cadastros/espacos/EditarEspacosCtrl.js
+++ src/app/pages/cadastros/espacos/EditarEspacosCtrl.js
@@ -12,13 +12,10 @@
     function EditarEspacosCtrl($scope, $http, $stateParams, CONFIG) {
 
         $http.get(CONFIG.dwdApi + '/listar-tabela-preco').then(function (response) {
-            console.log('tabela de preco');
-            console.log(response);
             $scope.tabelaPreco = response.data;
         });
 
         $http.get(CONFIG.dwdApi + '/espacos/' + $stateParams.id).then(function (response) {
-            console.log(response.data);
             $scope.espaco = {
                 id_espacos: response.data.id_espacos,
                 tx_razao_social: response.data.tx_razao_social,
@@ -48,17 +45,20 @@
                 tx_cargo: response.data.tx_cargo,
                 tx_email_comercial_resp: response.data.tx_email_comercial_resp,
                 tx_desc_ativ_resp: response.data.tx_desc_ativ_resp,
+                sala: {
+                    id_salas: null,
+                    tx_nome_salas: null,
+                    nr_metragem_salas: null,
+                    nr_altura_pe_direito_salas: null
+                },
 
             };
 
             $scope.tabelaPrecoEspaco = response.data.espacos_tabela;
 
-            console.log($scope.tabelaPrecoEspaco);
-
         });
 
         $scope.editarEspaco = function (espaco, tabelaPreco) {
-            console.log(espaco);
             $http.put(CONFIG.dwdApi + '/espacos/' + espaco.id_espacos, {
                 espaco: espaco,
                 tabelaPreco: tabelaPreco ? tabelaPreco : null
@@ -72,7 +72,7 @@
             });
         };
 
-        $scope.deletarEspacoTB = function (tb, espaco) {
+        $scope.deletarEspacoTB = function (tb) {
             swal({
                 title: "Você tem certeza?",
                 text: "Uma vez deletado, não é possível recuperar esta informação!",
@@ -82,21 +82,75 @@
             })
                 .then(function (willDelete) {
                     if (willDelete) {
-                        $http.delete(CONFIG.dwdApi + '/espacos/tabela-preco/' + tb + '/' + espaco).then(function (response) {
-                            console.log(response);
-                            if(response.data = 1) {
-                                swal("Parabéns!", "Sala deletada com sucesso!", "success")
-                                    .then(function () {
-                                        location.reload();
-                                    });
+                        $http.delete(CONFIG.dwdApi + '/espacos/tabela-preco/' + tb + '/' + $scope.espaco.id_espacos)
+                            .then(function (response) {
+                                console.log(response);
+                                if (response.data = 1) {
+                                    swal("Parabéns!", "Tabela de preço deletada com sucesso!", "success")
+                                        .then(function () {
+                                            location.reload();
+                                        });
 
-                            }
-                        });
+                                }
+                            });
                     } else {
                         // console.log('não faz nada');
                     }
                 });
-        }
+        };
+
+        /**
+         * Buscar as Salas já cadastradas em um espaço.
+         */
+        $http.get(CONFIG.dwdApi + '/espacos/salas/' + $stateParams.id).then(function (response) {
+            $scope.espacoSalaTable = response.data;
+        });
+
+        $scope.deletarSala = function (id) {
+            swal({
+                title: "Você tem certeza?",
+                text: "Uma vez deletado, não é possível recuperar esta informação!",
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+            })
+                .then(function (willDelete) {
+                    if (willDelete) {
+                        $http.delete(CONFIG.dwdApi + '/salas/' + id + '/' + $scope.espaco.id_espacos)
+                            .then(function (response) {
+                                if (response.data = 1) {
+                                    swal("Parabéns!", "Sala deletada com sucesso!", "success")
+                                        .then(function () {
+                                            location.reload();
+                                        });
+
+                                }
+                            });
+                    } else {
+                        // console.log('não faz nada');
+                    }
+                });
+        };
+
+        /**
+         * Lista uma sala especifica. É usado para mostrar o conteudo de uma sala ao editar.
+         */
+        $http.get(CONFIG.dwdApi + '/salas/' + $stateParams.id).then(function (response) {
+            $scope.sala = {
+                id_salas: response.data.id_salas,
+                tx_nome_salas: response.data.tx_nome_salas,
+                nr_metragem_salas: response.data.nr_metragem_salas,
+                nr_altura_pe_direito_salas: response.data.nr_altura_pe_direito_salas
+            };
+
+        });
+
+        $scope.editarSala = function (sala) {
+            $scope.espaco.sala.id_salas = sala.id_salas;
+            $scope.espaco.sala.tx_nome_salas = sala.tx_nome_salas;
+            $scope.espaco.sala.nr_metragem_salas = sala.nr_metragem_salas;
+            $scope.espaco.sala.nr_altura_pe_direito_salas = sala.nr_altura_pe_direito_salas;
+        };
     }
 
 })();
--- src/app/pages/config.js
+++ src/app/pages/config.js
@@ -1,8 +1,3 @@
-// angular.module('BlurAdmin',[]).constant("CONFIG", {
-//     dwdApi: "http://localhost:8000/api"
-//     // dwdApi: "http://dwddesenvolvimento-api.faromidia.com.br/api"
-// });
-
 /**
  * @author Alex Alexandre
  * created on 10/10/2018
@@ -11,13 +6,8 @@
     'use strict';
 
     angular.module('BlurAdmin.config', []).constant("CONFIG", {
-        // dwdApi: "http://localhost:8000/api"
-        dwdApi: "http://dwddesenvolvimento-api.faromidia.com.br/api"
+        dwdApi: "http://localhost:8000/api"
+        // dwdApi: "http://dwddesenvolvimento-api.faromidia.com.br/api"
     });
-
-    // /** @ngInject */
-    // function routeConfig($urlRouterProvider) {
-    //     $urlRouterProvider.otherwise('/dashboard');
-    // }
 
 })();
